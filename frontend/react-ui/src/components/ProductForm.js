fac8dd8f5015f0c31c5aedd99f5d8da7465f33f8
Mark Cheng <lujiang.cheng@siegefx.com>
2023-12-14 19:49:44 +0000

    React-ui - added product not found ui

--- frontend/react-ui/src/components/ProductForm.js
+++ frontend/react-ui/src/components/ProductForm.js
@@ -51,21 +51,39 @@ const ProductForm = () => {
 
   const navigate = useNavigate();
 
-  const isFormInValid = !product.title
-      || !product.category
+  const isFormInValid = !product.title.trim()
+      || !product.category.trim()
       || product.price < 1
-      || !product.description
+      || !product.description.trim()
       || (!neverExpires && !product.expiry);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (id) {
-      dispatch(updateProduct({ id: Number(id), ...product }));
+    if (existing) {
+      dispatch(updateProduct({ id: existing.id, ...product }));
     } else {
       dispatch(createProduct(product));
     }
     navigate('/');
   };
+
+  if (typeof id !== 'undefined' && !existing) {
+    return <div className={`d-flex flex-column justify-content-center align-items-center ${styles.notFound}`}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor"
+           className="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+        <path
+            d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
+        <path
+            d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+      </svg>
+      <div className="mt-3">
+        Product not found!
+      </div>
+      <div className="mt-3">
+        <button type="button" className="btn btn-secondary" onClick={() => navigate('/')}>Go to Homepage</button>
+      </div>
+    </div>;
+  }
 
   return (
     <div className={styles.container}>
